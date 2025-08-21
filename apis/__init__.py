ae06ccba4e494d324031b4897a077e0267a37d0f
TuanThanh2kk4 <tuanthanh2kk4@gmail.com>
2025-08-21 16:56:27 +0700

    feat: Add core API endpoints

---
+++ apis/__init__.py
@@ -0,0 +1,2 @@
+from apis.endpoints.core import router
+from apis.middlewares.auth import TokenAuthMiddleware
---
+++ apis/endpoints/core.py
@@ -0,0 +1,10 @@
+"""
+API endpoints for OASM Assistant
+"""
+import logging
+
+from fastapi import APIRouter
+
+
+logger = logging.getLogger(__name__)
+router = APIRouter()
