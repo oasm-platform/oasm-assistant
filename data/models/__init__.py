7397300f3d3ded967d0faadb892c11be91c6a0f7
TuanThanh2kk4 <tuanthanh2kk4@gmail.com>
2025-09-01 16:01:50 +0700

    feat: Remove database models and connection

--- data/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .asset import Asset
-from .http_response import HttpResponse
-from .port import Port
-from .views import IpAssetsView, StatusCodeAssetsView
-from .base import BaseEntity
-from .tls_info import TlsInfo
-from .knowledgebase_info import KnowledgebaseInfo
-
-__all__ = ["Asset", "HttpResponse", "Port", "IpAssetsView", "StatusCodeAssetsView", "BaseEntity", "TlsInfo", "KnowledgebaseInfo"]
--- data/repositories/__init__.py
+++
@@ -1,6 +0,0 @@
-from .asset_repository import AssetRepository
-from .http_response_repository import HttpResponseRepository
-from .port_repository import PortRepository
-from .view_repository import ViewRepository
-
-__all__ = ["AssetRepository", "HttpResponseRepository", "PortRepository", "ViewRepository"]
