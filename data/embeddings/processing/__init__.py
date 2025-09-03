79031d962fa199abbfe7c9988d718686ed6fea52
dovancuong12 <dovancuong3636@mail.com>
2025-09-03 16:20:37 +0700

    chore(processing): expose preprocessing and chunking classes in package init - Re-export TextPreprocessor & config - Re-export SentenceChunker & Chunk dataclass

---
+++ data/embeddings/processing/__init__.py
@@ -0,0 +1,9 @@
+from .text_preprocessor import TextPreprocessor, TextPreprocessorConfig
+from .chunk_processor import SentenceChunker, Chunk
+
+__all__ = [
+    "TextPreprocessor",
+    "TextPreprocessorConfig",
+    "SentenceChunker",
+    "Chunk",
+]
