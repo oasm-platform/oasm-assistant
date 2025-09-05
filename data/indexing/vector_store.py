f78306013783cbece7eb016dafe6790cee6a2a5d
dovancuong12 <dovancuong3636@mail.com>
2025-09-06 00:08:29 +0700

    feat(vector_store): add insert_data method for managing document insertion(#16)

--- data/indexing/vector_store.py
+++ data/indexing/vector_store.py
@@ -1,3 +1,20 @@
 """
 Vector database management
 """
+from data.database.chroma_database import ChromaDatabase
+
+class VectorStore:
+
+    def __init__(self, host="localhost", port=8000):
+
+        self.db = ChromaDatabase(host=host, port=port)
+
+    def insert_data(self, collection_name: str, documents: list, metadatas: list = None, embeddings: list = None):
+        self.db.create_collection(name=collection_name)
+        self.db.add_documents(
+            collection_name=collection_name,
+            documents=documents,
+            metadatas=metadatas,
+            embeddings=embeddings
+        )
+        print(f"Inserted {len(documents)} documents into collection '{collection_name}'.")
