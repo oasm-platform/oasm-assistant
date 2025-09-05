4625c883d0ce07bdb3a15c270eb9d9f7edc879fc
dovancuong12 <dovancuong3636@mail.com>
2025-09-05 21:03:20 +0700

    feat: add function to init

--- data/embeddings/processing/__init__.py
+++ data/embeddings/processing/__init__.py
@@ -1,9 +1,11 @@
 from .text_preprocessor import TextPreprocessor, TextPreprocessorConfig
-from .chunk_processor import SentenceChunker, Chunk
+from .chunk_processor import SentenceChunker, Chunk, SentenceChunkerConfig, WhitespaceTokenizer
 
 __all__ = [
     "TextPreprocessor",
     "TextPreprocessorConfig",
     "SentenceChunker",
     "Chunk",
+    "SentenceChunkerConfig",
+    "WhitespaceTokenizer",
 ]
