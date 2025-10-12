a7766eb13223484477201a13b977eb156847951e
TuanThanh2kk4 <tuanthanh2kk4@gmail.com>
2025-10-12 09:04:30 +0700

    chore(prompts): remove unused nuclei generation prompts module

--- llms/prompts/__init__.py
+++ llms/prompts/__init__.py
@@ -1,10 +1,8 @@
 from .domain_classification_prompts import DomainClassificationPrompts
-from .nuclei_generation_prompts import NucleiGenerationPrompts
 from .security_agent_prompts import SecurityAgentPrompts
 from .conversation_prompts import ConversationPrompts
 
 __all__ = [
-    "NucleiGenerationPrompts",
     "SecurityAgentPrompts",
     "DomainClassificationPrompts",
     "ConversationPrompts"
